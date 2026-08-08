52ad7d5bbeb2f812761b39c75f47b4263ab4013a
Joyeeta Das <joyeetadas597@gmail.com>
2026-08-08 14:03:32 +0530

    add dashbord to hero and done responsive also

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
 import { Hero } from '../../components/hero/hero';
-import { Dashboard } from '../../components/dashboard/dashboard';
 import { StatsSection } from '../../components/stats-section/stats-section';
 import { Pricing } from '../../components/pricing/pricing';
 import { Features } from '../../components/features/features';
@@ -10,7 +9,7 @@ import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Navbar,Hero,Dashboard,StatsSection,Pricing,Features,Cta,Footer],
+  imports: [Navbar,Hero,StatsSection,Pricing,Features,Cta,Footer],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
